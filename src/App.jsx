6b749d5bd5aa8e1ee571e6dae7a28e4a2450a06d
Keyla Juleyma Cardenas Esteban <keyla.16f@gmail.com>
2023-03-14 16:12:01 -0500

    buscando error

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from 'react-redux';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import {
+    increment,
+    decrement,
+    incrementBy2,
+    reset,
+} from './store/slices/counter';
+
+function App() {
+    const { counter } = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="App">
+            <div>
+                <a href="https://reactjs.org" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+                <p> count is {counter}</p>
+            </div>
+
+            <div className="card">
+                <button onClick={() => dispatch(increment())}>Increment</button>
+                <button onClick={() => dispatch(decrement())}>Decrement</button>
+                <button onClick={() => dispatch(incrementBy2(2))}>
+                    Increment by 2
+                </button>
+                <button onClick={() => dispatch(reset())}>Reset</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
